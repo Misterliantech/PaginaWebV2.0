d097cf332930af0992d8389c949cf99e5d23bb32
JulianChoXX <julianquinterortiz@gmail.com>
2024-09-24 21:54:09 -0500

    24/09/2024

--- app/server.js
+++ app/server.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 import {methods as authentication} from "./controllers/authentication.js"
+import exp from "constants";
 
 //Server
 const app = express();
@@ -14,9 +15,10 @@ console.log("Servidor corriendo en puerto #",app.get("port"));
 app.get("/", (req, res)=> res.sendFile(__dirname + "/pages/login.html"))
 app.get("/register", (req, res)=> res.sendFile(__dirname + "/pages/register.html"))
 app.get("/catalogo/catalogo", (req, res)=> res.sendFile(__dirname + "/pages/catalogo/catalogo.html"))
-app.get("/api/register",authentication.register);
-app.get("/api/login",authentication.login);
+app.post("/api/register",authentication.register);
+app.post("/api/login",authentication.login);
 
 //Configuracion
 
 app.use(express.static(__dirname + "/public"));
+app.use(express.json());
