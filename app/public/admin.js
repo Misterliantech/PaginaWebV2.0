02f4e7414b49934d44c4276fabf530a83d0eeb27
JulianChoXX <julianquinterortiz@gmail.com>
2024-10-10 21:54:41 -0500

    10/10/2024

--- app/public/admin.js
+++ app/public/admin.js
@@ -1,33 +1,41 @@
-document.getElementsByTagName("button")[0].addEventListener("click",()=>{
 
-    document.cookie ='jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    document.location.href = "/"
+document.getElementsByTagName("button")[0].addEventListener("click", () => {
+    document.cookie = 'jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.location.href = "/";
+});
 
-  })
 
-  let index = 0;
-  const productos = document.querySelector('.contenedor');
-  const totalProductos = document.querySelectorAll('.producto').length;
-  const productosVisibles = 4;
-  
-  function cambiar(direccion) {
-      index += direccion;
-  
-      if (index < 0) {
-          index = 0; 
-      } else if (index > totalProductos - productosVisibles) {
-          index = totalProductos - productosVisibles;
-          index = -4;
-      }
-      actualizarCarrusel();
-  }
-  
-  function actualizarCarrusel() {
-      const desplazamiento = index * -420; 
-      productos.style.transform = `translateX(${desplazamiento}px)`;
-  }
-  
+let index = 0;
+const productos = document.querySelector('.contenedor');
+const totalProductos = document.querySelectorAll('.producto').length;
+const productosVisibles = 4;
 
-  setInterval(() => cambiar(1), 3500);
+
+function cambiar(direccion) {
+    index += direccion;
+
+ 
+    if (index < 0) {
+        index = 0; 
+    } 
+
+    else if (index > totalProductos - productosVisibles) {
+        index = totalProductos - productosVisibles;
+        index = 0;
+    }
+
+
+    actualizarCarrusel();
+}
+
+
+function actualizarCarrusel() {
+    const desplazamiento = index * -300; 
+    productos.style.transform = `translateX(${desplazamiento}px)`;
+}
+
+
+setInterval(() => cambiar(1), 3500);
+
   
   
