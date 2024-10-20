ebb077ff67ea67a835a26ef08ba65c4bbe3edda9
JulianChoXX <julianquinterortiz@gmail.com>
2024-10-19 22:28:16 -0500

    19/10/24

--- app/index.js
+++ app/index.js
@@ -7,23 +7,22 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 import {methods as authentication} from "./controllers/authentication.controller.js"
 import {methods as authorization} from "./middlewares/authorization.js";
 
-//Servidor
-
 const app = express();
 app.set("port",4000);
-app.listen(app.get("port"));
-console.log("Servidor corriendo en puerto",app.get("port"));
-
-// Configuracion
+app.listen(app.get("port"), () => {
+  console.log("Servidor corriendo en puerto", app.get("port"));
+});
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(cookieParser());
 
-//Rutas
-
-app.get("/",authorization.soloPublico, (req,res)=> res.sendFile(__dirname + "/pages/login.html"));
+app.get("/",authorization.soloPublico,(req,res)=> res.sendFile(__dirname + "/pages/login.html"));
 app.get("/register",authorization.soloPublico,(req,res)=> res.sendFile(__dirname + "/pages/register.html"));
+app.get("/compras", authorization.soloAdmin, (req, res) => {
+    console.log("Llegó a la ruta /compras");
+    res.sendFile(__dirname + "/pages/compras.html");
+  });
 app.get("/admin",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname + "/pages/admin/admin.html"));
 app.post("/api/login",authentication.login);
 app.post("/api/register",authentication.register);
--- app/middlewares/authorization.js
+++ app/middlewares/authorization.js
@@ -4,31 +4,36 @@ import {usuarios} from "./../controllers/authentication.controller.js";
 
 dotenv.config();
 
-function soloAdmin(req,res,next){
+function soloAdmin(req, res, next, decodificada) {
   const logueado = revisarCookie(req);
-  if(logueado) return next();
-  return res.redirect("/")
-}
-
-function soloPublico(req,res,next){
-  const logueado = revisarCookie(req);
-  if(!logueado) return next();
-  return res.redirect("/admin")
-}
-
-function revisarCookie(req){
-  try{
-    const cookieJWT = req.headers.cookie.split("; ").find(cookie => cookie.startsWith("jwt=")).slice(4);
-    const decodificada = jsonwebtoken.verify(cookieJWT,process.env.JWT_SECRET);
-    console.log(decodificada)
-    const usuarioAResvisar = usuarios.find(usuario => usuario.user === decodificada.user);
-    console.log(usuarioAResvisar)
-    if(!usuarioAResvisar){
-      return false
-    }
-    return true;
+  if (!logueado) return res.redirect("/");
+  const usuario = usuarios.find(usuario => usuario.user === decodificada.user);
+  if (usuario && usuario.rol === "admin") {
+    next();
+  } else {
+    res.status(403).send("No tienes permiso para acceder a esta página");
   }
-  catch{
+}
+
+function soloPublico(req, res, next) {
+  const logueado = revisarCookie(req);
+  if (logueado) {
+    return res.redirect("/admin");
+  } else {
+    return next();
+  }
+}
+
+function revisarCookie(req) {
+  try {
+    const cookieJWT = req.headers.cookie.split("; ").find(cookie => cookie.startsWith("jwt=")).slice(4);
+    const decodificada = jsonwebtoken.verify(cookieJWT, process.env.JWT_SECRET);
+    const usuarioAResvisar = usuarios.find(usuario => usuario.user === decodificada.user);
+    if (!usuarioAResvisar) {
+      return false;
+    }
+    return decodificada;
+  } catch {
     return false;
   }
 }
